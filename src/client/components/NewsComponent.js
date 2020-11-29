9aac2bf3e2e5fc5fb6c35045aa3ecf970bc83040
RituPanchal <rpanchal1899@gmail.com>
2020-11-29 19:32:54 +0000

    feat: add line chart

--- src/client/components/NewsComponent.js
+++ src/client/components/NewsComponent.js
@@ -3,6 +3,10 @@ import "../styles/styles.css";
 import { connect } from 'react-redux';
 import { fetchNews, upVoteCount } from '../actions';
 
+import { LineChart } from 'react-chartkick';
+import 'chart.js'
+
+
 const Link = ({ url, title }) => (
     <a href={url} target="_blank" rel="noreferrer">
         <span>{title} </span>
@@ -72,55 +76,86 @@ class NewsComponent extends React.Component {
         );
     }
 
+    renderLineChart = results => {
+        const statistics = [];
+
+        for (let i = 0; i < results.length; i++) {
+            const id = results[i].id.toString();
+            const votes = results[i].vote_count;
+            console.log(id + " == " + votes);
+
+            const item = { [id]: votes }
+            statistics.push(item);
+        }
+        let graph_item = JSON.stringify(statistics);
+        graph_item = graph_item.replace(/[{}]/g, "");
+        graph_item = graph_item.replace('[', '{');
+        graph_item = graph_item.replace(']', '}');
+
+        var data = JSON.parse(graph_item);
+        console.log(graph_item, data);
+
+        return (
+            <LineChart
+                data={data}
+            />
+        )
+    }
+
     render() {
         const results = JSON.parse(JSON.stringify(this.props.news)).news;
         return (
-            <div className="c-news">
-                <div className="c-news__wrapper">
-                    <table>
-                        <colgroup>
-                            <col span="1" className="c-news__col--1" />
-                            <col span="1" className="c-news__col--2" />
-                            <col span="1" className="c-news__col--3" />
-                            <col span="1" className="c-news__col--4" />
-                            <col span="1" className="c-news__col--5" />
-                        </colgroup>
+            <>
+                <div className="c-news">
+                    <div className="c-news__wrapper">
+                        <table>
+                            <colgroup>
+                                <col span="1" className="c-news__col--1" />
+                                <col span="1" className="c-news__col--2" />
+                                <col span="1" className="c-news__col--3" />
+                                <col span="1" className="c-news__col--4" />
+                                <col span="1" className="c-news__col--5" />
+                            </colgroup>
 
-                        <tr className="c-news__header">
-                            <th>Comments</th>
-                            <th>Votes</th>
-                            <th>UpVote</th>
-                            <th>News Feed</th>
-                            <th>Time Posted</th>
-                        </tr>
-                        {
-                            results && results.map((item) => (
-                                <tr key={item.id} className="c-news__feed">
-                                    <td><span className="c-news__feed--comments">{item.comments}</span></td>
-                                    <td>{this.renderVoteCount(item)}</td>
-                                    <td>
-                                        <a href="/" onClick={this.setUpVoteCount.bind(this, item)}><span className="c-news__details--upvote">&#9650;</span></a>
-                                    </td>
-                                    <td className="c-news__details">
-                                        <span className="c-news__details--title">
-                                            <Link url={item.url} title={item.title} />
-                                        </span>
-                                        <span className="c-news__details--url">
-                                            <Link url={item.url} title={`(${this.getHostname(item.url)})`} />
-                                        </span>
-                                        <span className="c-news__details--author">
-                                            <Link url={`https://news.ycombinator.com/user?id=${item.author}`} title={`by ${item.author}`} />
-                                        </span>
-                                    </td>
-                                    <td>
-                                        <span className="c-news__details--time">{item.time} hours ago</span>
-                                    </td>
-                                </tr>
-                            ))
-                        }
-                    </table>
+                            <tr className="c-news__header">
+                                <th>Comments</th>
+                                <th>Votes</th>
+                                <th>UpVote</th>
+                                <th>News Feed</th>
+                                <th>Time Posted</th>
+                            </tr>
+                            {
+                                results && results.map((item) => (
+                                    <tr key={item.id} className="c-news__feed">
+                                        <td><span className="c-news__feed--comments">{item.comments}</span></td>
+                                        <td>{this.renderVoteCount(item)}</td>
+                                        <td>
+                                            <a href="/" onClick={this.setUpVoteCount.bind(this, item)}><span className="c-news__details--upvote">&#9650;</span></a>
+                                        </td>
+                                        <td className="c-news__details">
+                                            <span className="c-news__details--title">
+                                                <Link url={item.url} title={item.title} />
+                                            </span>
+                                            <span className="c-news__details--url">
+                                                <Link url={item.url} title={`(${this.getHostname(item.url)})`} />
+                                            </span>
+                                            <span className="c-news__details--author">
+                                                <Link url={`https://news.ycombinator.com/user?id=${item.author}`} title={`by ${item.author}`} />
+                                            </span>
+                                        </td>
+                                        <td>
+                                            <span className="c-news__details--time">{item.time} hours ago</span>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </table>
+                    </div>
+                </div >
+                <div>
+                    {this.renderLineChart(results)}
                 </div>
-            </div >
+            </>
         );
     }
 }
